694b26e1c7001c6df72e089581e0bec478cd2bc4
tacree <tom.acree@gmail.com>
2016-11-13 15:03:12 -0500

    basic root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  bootstrap: [AppComponent],
+
+  // we'll look at providers more in depth in later examples
+  providers: []
 })
 export class AppModule { }
